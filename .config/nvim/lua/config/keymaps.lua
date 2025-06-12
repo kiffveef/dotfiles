9e013e44b0fddd32aa99cab786c0a2c12c28e241
kiffveef <fuzuki.close+3@gmail.com>
2025-06-12 20:55:03 +0900

    Add nvim insert mode keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,7 @@ local keymap = vim.api.nvim_set_keymap
 -- ; と : を入れ替え
 keymap("n", ";", ":", opts)
 
+-- 検索ハイライトOFF
 keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", opts)
 
 -- インサートモード離脱
@@ -16,11 +17,17 @@ keymap("i", "kj", "<Esc>", opts)
 -- スペース挿入
 keymap("i", ",", ",<Space>", opts)
 
+-- インサートモード中のカーソル移動
+keymap("i", "<C-k>", "<Up>", opts)
+keymap("i", "<C-j>", "<Down>", opts)
+keymap("i", "<C-l>", "<Right>", opts)
+keymap("i", "<C-h>", "<Left>", opts)
+
 vim.keymap.set("c", "<c-n>", function()
-  return vim.fn.wildmenumode() == 1 and "<c-n>" or "<down>" end ,
+  return vim.fn.wildmenumode() == 1 and "<c-n>" or "<down>" end,
   {expr = true, desc = "Select next completion item"})
 vim.keymap.set("c", "<c-p>", function()
-  return vim.fn.wildmenumode() == 1 and "<c-p>" or "<up>" end ,
+  return vim.fn.wildmenumode() == 1 and "<c-p>" or "<up>" end,
   {expr = true, desc = "Select previous completion item"})
 vim.keymap.set('n', '<space>q', function()
   if not pcall(vim.cmd.tabclose) then
